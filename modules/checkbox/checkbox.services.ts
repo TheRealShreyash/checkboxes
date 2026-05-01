367583ee5f30b22ea13a3004c3febc0f3fde0556
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-05-01 17:33:06 +0530

    remove unnecessary variable

--- modules/checkbox/checkbox.services.ts
+++ modules/checkbox/checkbox.services.ts
@@ -2,7 +2,6 @@ import ApiError from "../../common/utils/api-error";
 import { redis } from "../../redis-connection";
 
 const CHECKBOX_STATE_KEY = process.env.CHECKBOX_STATE_KEY!;
-const CHECKBOX_SIZE = parseInt(process.env.CHECKBOX_SIZE!);
 
 export const getState = async () => {
   const existingState = await redis.get(CHECKBOX_STATE_KEY);

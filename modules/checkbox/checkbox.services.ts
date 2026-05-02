4d41c7355ccd29d128266edaac9a4ce7e54f8c36
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-05-02 16:56:53 +0530

    add: mock states if no state found

--- modules/checkbox/checkbox.services.ts
+++ modules/checkbox/checkbox.services.ts
@@ -2,11 +2,18 @@ import ApiError from "../../common/utils/api-error";
 import { redis } from "../../redis-connection";
 
 const CHECKBOX_STATE_KEY = process.env.CHECKBOX_STATE_KEY!;
+const CHECKBOX_SIZE = parseInt(process.env.CHECKBOX_SIZE!);
 
 export const getState = async () => {
   const existingState = await redis.get(CHECKBOX_STATE_KEY);
 
-  if (!existingState) throw ApiError.notFound("No state found");
+  if (!existingState) {
+    await redis.set(
+      CHECKBOX_STATE_KEY,
+      JSON.stringify(new Array(CHECKBOX_SIZE).fill(false)),
+    );
+    throw ApiError.notFound("No state found");
+  }
 
   const rawData = JSON.parse(existingState);
   return rawData;

13315666c1accb8879bae1ea7703ecb8871e08a7
TheRealShreyash <koshta.shreyash02@gmail.com>
2026-05-02 20:37:50 +0530

    chore: test production

--- modules/auth/auth.services.ts
+++ modules/auth/auth.services.ts
@@ -31,11 +31,15 @@ export const refreshTokens = async (refreshToken: string) => {
     }),
   });
 
+  console.log(res)
+
   if (!res.ok) throw ApiError.unauthorized("Failed to refresh tokens");
 
   const { data } = (await res.json()) as {
     data: { accessToken: string; refreshToken: string };
   };
+
+  console.log(data)
 
   return data;
 };
